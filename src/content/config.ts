fa5d97b2919e19540c16e1a66520bf911d0f849b
Djole <0xdjole@gmail.com>
2025-11-02 04:28:11 +0100

    no log

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,6 @@
 import { defineCollection } from "astro:content";
 import { docsSchema } from "@astrojs/starlight/schema";
 
-console.log("log");
 export const collections = {
   docs: defineCollection({ schema: docsSchema() }),
 };
